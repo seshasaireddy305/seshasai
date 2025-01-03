6649307064e16454b3372f2b0437caede7374a71
seshasaireddy305 <seshasaireddy121@gmail.com>
2025-01-03 19:34:12 +0000

    added iam role

--- main.tf
+++ main.tf
@@ -15,41 +15,36 @@ resource "aws_iam_role" "example" {
   name = "oidc-role"
 
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Federated": "arn:aws:iam::529088270180:oidc-provider/token.actions.githubusercontent.com" # Change OIDC ARN
-            },
-            "Action": [
-                "iam:CreateOpenIDConnectProvider",
-                "iam:CreateRole",
-                "iam:CreatePolicy",
-               "sts:AssumeRoleWithWebIdentity"
-              ],
-              "Resource": "*"
-            "Condition": {
-                "StringEquals": {
-                    "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
-                },
-                "StringLike": {
-                    "token.actions.githubusercontent.com:sub": "repo:seshasaireddy305/seshasai:*" # Change repo anme
-                },
-                "ForAllValues:StringEquals": {
-                    "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
-                    "token.actions.githubusercontent.com:iss": "https://token.actions.githubusercontent.com" 
-                }
-      }
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::529088270180:oidc-provider/token.actions.githubusercontent.com" # Change OIDC ARN and Remove this Commit
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : "repo:seshasaireddy305/seshasai:*" # Change repo anme and Remove this Commit
+          },
+          "ForAllValues:StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com",
+            "token.actions.githubusercontent.com:iss" : "https://token.actions.githubusercontent.com"
+          }
+        }
       }
     ]
   })
 }
 
+
 resource "aws_iam_policy" "example" {
   name        = "oidc-policy"
   description = "A test policy for OIDC"
-  policy      = jsonencode({
+  policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
