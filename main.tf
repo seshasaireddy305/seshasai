e81afbb73cfa1e54df5baacd3c1c98dd6ffe373b
seshasaireddy305 <seshasaireddy121@gmail.com>
2025-01-04 19:02:41 +0000

    adding pool

--- main.tf
+++ main.tf
@@ -8,58 +8,38 @@ resource "aws_iam_role" "example" {
   name = "oidc_test"
 
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    Version = "2012-10-17",
+    Statement = [
       {
-        "Effect": "Allow",
-        "Principal": {
-          "Federated": "arn:aws:iam::529088270180:oidc-provider/token.actions.githubusercontent.com"
+        Effect = "Allow",
+        Principal = {
+          Federated = "cognito-identity.amazonaws.com"
         },
-        "Action": "sts:AssumeRoleWithWebIdentity",
-        "Condition": {
-          "StringEquals": {
-            "cognito-identity.amazonaws.com:aud": "us-east-2:529088270180*"
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "cognito-identity.amazonaws.com:aud" = "us-east-1:be717451-427f-4460-a49f-b705da56bd39" # Replace with the exact Identity Pool ID
           },
-          "ForAnyValue:StringLike": {
-            "cognito-identity.amazonaws.com:amr": "unauthenticated"
+          "ForAnyValue:StringLike" = {
+            "cognito-identity.amazonaws.com:amr" = "unauthenticated"
           }
         }
       },
       {
-        "Effect": "Allow",
-      "Principal": {
-        "Federated": "arn:aws:iam::529088270180:oidc-provider/token.actions.githubusercontent.com"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+        Effect = "Allow",
+        Principal = {
+          Federated = "arn:aws:iam::529088270180:oidc-provider/token.actions.githubusercontent.com"
         },
-        "StringLike": {
-          "token.actions.githubusercontent.com:sub": "repo:seshasaireddy305/seshasai:*"
-        }
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          },
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:seshasaireddy305/seshasai:*"
+          }
         }
       }
     ]
   })
-}
-
-resource "aws_iam_policy" "example" {
-  name        = "oidc-policy"
-  description = "A test policy for OIDC"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = ["s3:ListBucket"],
-        Resource = ["arn:aws:s3:::example-bucket"]
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "example" {
-  role       = aws_iam_role.example.name
-  policy_arn = aws_iam_policy.example.arn
 }
