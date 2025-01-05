fc0d76acfad14fe6adb71040f8fcb1e7c3a06039
seshasaireddy305 <seshasaireddy121@gmail.com>
2025-01-05 16:38:41 +0000

    grn

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
+  url             = "aws_iam_openid_connect_provider.github.arn"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
