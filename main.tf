575c67e5d08d2db6278858bdd57bf009cb94ae14
seshasaireddy305 <seshasaireddy121@gmail.com>
2025-01-06 17:42:50 +0000

    min.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 data "aws_iam_openid_connect_provider" "existing" {
   url = "https://token.actions.githubusercontent.com"
 }
-
+resource "aws_iam_openid_connect_provider" "github" {
+  count           = length(data.aws_iam_openid_connect_provider.existing.id) == 0 ? 1 : 0
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
 data "aws_iam_policy_document" "github_actions_assume_role" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
