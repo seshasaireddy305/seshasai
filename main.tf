423c539a6114dd833dabdb724581ed98cddba574
seshasaireddy305 <seshasaireddy121@gmail.com>
2025-01-06 17:59:40 +0000

    removed resource

--- main.tf
+++ main.tf
@@ -49,11 +49,11 @@ data "aws_iam_policy_document" "github_actions" {
   }
 }
 
-resource "aws_iam_policy" "github-actions" {
-  name        = "github"
-  description = "Grant Github Actions the ability to push to ECR"
-  policy      = data.aws_iam_policy_document.github_actions.json
-}
+# resource "aws_iam_policy" "github-actions" {
+#   name        = "github"
+#   description = "Grant Github Actions the ability to push to ECR"
+#   policy      = data.aws_iam_policy_document.github_actions.json
+# }
 
 resource "aws_iam_role_policy_attachment" "github_actions" {
   role       = aws_iam_role.github_actions.name
