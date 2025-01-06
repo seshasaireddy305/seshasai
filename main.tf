c8febd62f1cf7cd42d4a5df91c6c23ea43620bed
seshasaireddy305 <seshasaireddy121@gmail.com>
2025-01-06 17:11:39 +0000

    oidc-provider

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "aws_iam_openid_connect_provider" "default" {
-  url = "https://accounts.google.com"
+data "aws_iam_openid_connect_provider" "existing" {
+  url = "https://token.actions.githubusercontent.com"
+}
 
-  client_id_list = [
-    "266362248691-342342xasdasdasda-apps.googleusercontent.com",
-  ]
-
-  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
-
+resource "aws_iam_openid_connect_provider" "github" {
+  count           = length(data.aws_iam_openid_connect_provider.existing.id) == 0 ? 1 : 0
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 data "aws_iam_policy_document" "github_actions_assume_role" {
