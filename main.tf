abd6cb772b08eedc1cd0662c44d86a32cfdc8aae
seshasaireddy305 <seshasaireddy121@gmail.com>
2025-01-06 18:02:27 +0000

    removed resource

--- main.tf
+++ main.tf
@@ -57,5 +57,5 @@ data "aws_iam_policy_document" "github_actions" {
 
 resource "aws_iam_role_policy_attachment" "github_actions" {
   role       = aws_iam_role.github_actions.name
-  policy_arn = aws_iam_policy.github-actions.arn
+  policy_arn = aws_iam_policy.github_actions.arn
 }
