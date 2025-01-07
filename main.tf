e28cf694e2407ab2ede4bb9fc3466b04233b1d1b
seshasaireddy305 <seshasaireddy121@gmail.com>
2025-01-07 10:36:11 +0000

    ec2

--- main.tf
+++ main.tf
@@ -1,61 +1,85 @@
-data "aws_iam_openid_connect_provider" "existing" {
-  arn = "arn:aws:iam::529088270180:oidc-provider/token.actions.githubusercontent.com"
-}
-data "aws_iam_policy_document" "github_actions_assume_role" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    principals {
-      type        = "Federated"
-      identifiers = [data.aws_iam_openid_connect_provider.existing.arn]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "token.actions.githubusercontent.com:aud"
-      values   = ["sts.amazonaws.com"]
-    }
-    condition {
-      test     = "StringLike"
-      variable = "token.actions.githubusercontent.com:sub"
-      values = [
-        "repo:org1/*:*",
-        "repo:org2/*:*"
-      ]
-    }
-  }
-}
-
-resource "aws_iam_role" "github_actions" {
-  name               = "github-actions"
-  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
-}
-
-data "aws_iam_policy_document" "github_actions" {
-  statement {
-    actions = [
-      "ecr:BatchGetImage",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:CompleteLayerUpload",
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:InitiateLayerUpload",
-      "ecr:PutImage",
-      "ecr:UploadLayerPart",
-    ]
-    resources = ["*"]
-    condition {
-      test     = "StringEquals"
-      variable = "aws:ResourceTag/permit-github-action"
-      values   = ["true"]
-    }
-  }
-}
-
-# resource "aws_iam_policy" "github-actions" {
-#   name        = "github"
-#   description = "Grant Github Actions the ability to push to ECR"
-#   policy      = data.aws_iam_policy_document.github_actions.json
+# data "aws_iam_openid_connect_provider" "existing" {
+#   arn = "arn:aws:iam::529088270180:oidc-provider/token.actions.githubusercontent.com"
+# }
+# data "aws_iam_policy_document" "github_actions_assume_role" {
+#   statement {
+#     actions = ["sts:AssumeRoleWithWebIdentity"]
+#     principals {
+#       type        = "Federated"
+#       identifiers = [data.aws_iam_openid_connect_provider.existing.arn]
+#     }
+#     condition {
+#       test     = "StringEquals"
+#       variable = "token.actions.githubusercontent.com:aud"
+#       values   = ["sts.amazonaws.com"]
+#     }
+#     condition {
+#       test     = "StringLike"
+#       variable = "token.actions.githubusercontent.com:sub"
+#       values = [
+#         "repo:org1/*:*",
+#         "repo:org2/*:*"
+#       ]
+#     }
+#   }
 # }
 
-resource "aws_iam_role_policy_attachment" "github_actions" {
-  role       = aws_iam_role.github_actions.name
-  policy_arn = aws_iam_policy.github_actions.arn
+# resource "aws_iam_role" "github_actions" {
+#   name               = "github-actions"
+#   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+# }
+
+# data "aws_iam_policy_document" "github_actions" {
+#   statement {
+#     actions = [
+#       "ecr:BatchGetImage",
+#       "ecr:BatchCheckLayerAvailability",
+#       "ecr:CompleteLayerUpload",
+#       "ecr:GetDownloadUrlForLayer",
+#       "ecr:InitiateLayerUpload",
+#       "ecr:PutImage",
+#       "ecr:UploadLayerPart",
+#     ]
+#     resources = ["*"]
+#     condition {
+#       test     = "StringEquals"
+#       variable = "aws:ResourceTag/permit-github-action"
+#       values   = ["true"]
+#     }
+#   }
+# }
+
+# # resource "aws_iam_policy" "github-actions" {
+# #   name        = "github"
+# #   description = "Grant Github Actions the ability to push to ECR"
+# #   policy      = data.aws_iam_policy_document.github_actions.json
+# # }
+
+# resource "aws_iam_role_policy_attachment" "github_actions" {
+#   role       = aws_iam_role.github_actions.name
+#   policy_arn = aws_iam_policy.github_actions.arn
+# }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
