af8be24c912ab183b7249651938ed584c397086a
seshasaireddy305 <seshasaireddy121@gmail.com>
2025-01-07 11:05:28 +0000

    s3

--- main.tf
+++ main.tf
@@ -83,3 +83,11 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket1342434"
+
+  tags = {
+    Name        = "My bucket"
+  }
+}
