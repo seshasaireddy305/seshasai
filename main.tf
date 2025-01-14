88aaa07b7b1c8f387c822a58d36a0cb448f3692e
seshasaireddy305 <seshasaireddy121@gmail.com>
2025-01-14 19:45:47 +0000

    error

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
 # Create a VPC
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
   tags = {
-    Name = "MainVPC"
+    Name = "tf-example"
   }
 }
 
 # Create public and private subnets
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-1a"
+
   tags = {
     Name = "PublicSubnet"
   }
@@ -91,10 +90,16 @@ resource "aws_key_pair" "deployer" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
 }
 
-resource "aws_instance" "WebServerInstance" {
-  ami           = "ami-12345678"
+resource "aws_instance" "foo" {
+  ami           = "ami-005e54dee72cc1d00" # us-west-2
   instance_type = "t2.micro"
 
-  subnet_id              = aws_subnet.public     # Keep this for VPC
-  vpc_security_group_ids = [aws_security_group.ec2_sg]  # Use Security Group ID here
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
 }
