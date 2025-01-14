10625d4a0967c5ad7731d4e9ad9203b66b963f78
seshasaireddy305 <seshasaireddy121@gmail.com>
2025-01-14 19:33:24 +0000

    ec2

--- main.tf
+++ main.tf
@@ -91,17 +91,10 @@ resource "aws_key_pair" "deployer" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
 }
 
-# Create an EC2 instance
-resource "aws_instance" "Web" {
-  ami           = "ami-0c02fb55956c7d316" # Update with a valid AMI ID for your region
+resource "aws_instance" "WebServerInstance" {
+  ami           = "ami-12345678"
   instance_type = "t2.micro"
-  # subnet_id     = aws_subnet.public.id
-  security_groups = [
-    aws_security_group.ec2_sg.name,
-  ]
-  key_name      = aws_key_pair.deployer.key_name
 
-  tags = {
-    Name = "Hellow-world"
-  }
+  subnet_id              = aws_subnet.public      # Keep this for VPC
+  vpc_security_group_ids = aws_security_group.ec2_sg   # Use Security Group ID here
 }
